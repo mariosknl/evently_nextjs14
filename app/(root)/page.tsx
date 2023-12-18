65098380d37727b9431d0f99ca809b3b1ccc6df4
marios-kanellopoulos <marios.kanellopoulos@vodafone.com>
2023-12-18 15:03:39 +0200

    search functionality implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,20 @@
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+	const page = Number(searchParams?.page) || 1;
+	const searchText = (searchParams?.query as string) || "";
+	const category = (searchParams?.category as string) || "";
+
 	const events = await getAllEvents({
-		query: "",
-		category: "",
-		page: 1,
+		query: searchText,
+		category,
+		page,
 		limit: 6,
 	});
 
@@ -48,7 +54,7 @@ export default async function Home() {
 				</h2>
 
 				<div className="w-full flex flex-col gap-5 md:flex-row">
-					Search CategoryFilter
+					<Search />
 				</div>
 
 				<Collection
