d5bff6333a68f3721104c85fc35b92cf7caf1074
marios-kanellopoulos <marios.kanellopoulos@vodafone.com>
2023-12-18 12:54:17 +0200

    related events

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,6 +1,9 @@
 // import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
-import { getEventById } from "@/lib/actions/event.actions";
+import {
+	getEventById,
+	getRelatedEventsByCategory,
+} from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
@@ -11,11 +14,11 @@ const EventDetails = async ({
 }: SearchParamProps) => {
 	const event = await getEventById(id);
 
-	// const relatedEvents = await getRelatedEventsByCategory({
-	// 	categoryId: event.category._id,
-	// 	eventId: event._id,
-	// 	page: searchParams.page as string,
-	// });
+	const relatedEvents = await getRelatedEventsByCategory({
+		categoryId: event.category._id,
+		eventId: event._id,
+		page: searchParams.page as string,
+	});
 
 	return (
 		<>
@@ -99,7 +102,7 @@ const EventDetails = async ({
 			{/* EVENTS with the same category */}
 			<section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
 				<h2 className="h2-bold">Related Events</h2>
-				{/*
+
 				<Collection
 					data={relatedEvents?.data}
 					emptyTitle="No Events Found"
@@ -108,7 +111,7 @@ const EventDetails = async ({
 					limit={3}
 					page={searchParams.page as string}
 					totalPages={relatedEvents?.totalPages}
-				/> */}
+				/>
 			</section>
 		</>
 	);
