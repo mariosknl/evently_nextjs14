0ab07306ab224fe3144f945c26cff79d725f8f56
marios-kanellopoulos <marios.kanellopoulos@vodafone.com>
2023-12-18 15:11:27 +0200

    categoryFilter implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
@@ -55,6 +56,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
 
 				<div className="w-full flex flex-col gap-5 md:flex-row">
 					<Search />
+					<CategoryFilter />
 				</div>
 
 				<Collection
